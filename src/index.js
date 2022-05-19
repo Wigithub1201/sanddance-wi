8f5b32d8759e8801a0d509560d437c408edb6397
meek <956642254@qq.com>
2022-05-19 19:10:21 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as deck from '@deck.gl/core';
 import * as layers from '@deck.gl/layers';
 import * as luma from '@luma.gl/core';
@@ -11,30 +11,52 @@ import './sanddance-app-site.css';
 import './sanddance-explorer.css';
 
 const Visualization = (props) => {
-	const data = [
-		{ a: 1, b: "c1" },
-		{ a: 1, b: "c2" },
-		{ a: 2, b: "c3" },
-		{ a: 3, b: "c4" }
-	];
+	let fileSrc = 'https://microsoft.github.io/SandDance/sample-data/demovote.tsv';
+
+	const [tableData, setTableData] = useState([]);
 
 	useEffect(() => {
-		if (data.length > 0) {
+		handleData({ dataUrl: fileSrc, type: 'tsv' })
+	}, []);
+
+	const handleData = (dataFile) => {
+		let loader = vega.loader();
+
+		function handleRawText(text) {
+			let data = vega.read(text, {
+				type: dataFile.type,
+				parse: 'auto'
+			});
+			setTableData(data)
+		}
+		loader.load(dataFile.dataUrl).then(handleRawText);
+	}
+
+	useEffect(() => {
+		if (tableData.length > 0) {
 			const explorerProps = {
 				logoClickUrl: 'javascript:;',
 				logoClickTarget: '_self',
 				mounted: explorer => {
-					explorer.load(data);
+					explorer.load(tableData);
 				}
 			};
 			fluentui.initializeIcons();
 			use(fluentui, React, ReactDOM, vega, deck, layers, luma);
 			ReactDOM.render(React.createElement(Explorer, explorerProps), document.getElementById('app'));
 		}
-	}, [])
+	}, [tableData])
 
 	return (
 		<>
+			<header style={{ backgroundColor: '#1293f9', height: '60px' }}></header>
+			<section style={{ backgroundColor: '#f7f7f7', color: 'teal', padding: '15px' }}>
+				sanddance - the core SandDance visualization canvas.
+				sanddance-react - the core SandDance visualization canvas for use in React based applications.
+				sanddance-vue - the core SandDance visualization canvas for use in Vue based applications.
+				sanddance-explorer - the core SandDance visualization canvas with UI to enable data exploration, for use in React based applications.
+				Publications
+			</section>
 			<div id="app"></div>
 		</>
 	)
